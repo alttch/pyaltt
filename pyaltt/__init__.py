58836b13617415db29eec38dcd59b0a61ee92a5e
divisor <div@altertech.com>
2019-03-02 03:41:26 +0100

    g as thread local

--- pyaltt/__init__.py
+++ pyaltt/__init__.py
@@ -1,4 +1,8 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
+
+import threading
 
 from pyaltt.workers import background_worker
 from pyaltt.functools import FunctionCollecton
+
+g = threading.local()
